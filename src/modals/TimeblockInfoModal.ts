eacadb4a31fd1ad4dab901722d00774d06fcf0b6
Lorite <a.lorite.mora@gmail.com>
2026-03-08 16:04:54 +0100

    feat: add setting for attachment search order in timeblock modal

--- src/modals/TimeblockInfoModal.ts
+++ src/modals/TimeblockInfoModal.ts
@@ -141,6 +141,9 @@ export class TimeblockInfoModal extends Modal {
 						}, {
 							placeholder: "Search files or type to create new...",
 							filter: "all",
+							sortOrder:
+								this.plugin.settings.calendarViewSettings
+									.timeblockAttachmentSearchOrder,
 						});
 					});
 			});
